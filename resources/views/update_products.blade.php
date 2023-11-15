a75ff9108eb0afad2f552b1f83fc323f940e5e2a
jaldrinportugal <131321321+jaldrinportugal@users.noreply.github.com>
2023-11-15 12:55:24 +0800

    gumagana

--- resources/views/update_products.blade.php
+++ resources/views/update_products.blade.php
@@ -8,19 +8,19 @@
             <div class="row">
                 <div class="col mb-3">
                     <label for="p_name" class="form-label">Product Name</label>
-                    <input type="text" class="form-control" name="update_product_name" value="{{$product->product_name}}">
+                    <input type="text" class="form-control" name="update_product_name" value="{{$products->product_name}}">
                 </div>
                 <div class="col-md-3 mb-3">
                     <label for="description" class="form-label">Description</label>
-                    <input type="text" class="form-control" name="update_description" value="{{$product->description}}">
+                    <input type="text" class="form-control" name="update_description" value="{{$products->description}}">
                 </div>
                 <div class="col mb-3">
                     <label for="price" class="form-label">Price</label>
-                    <input type="number" class="form-control" name="update_price"  value="{{$product->price}}">
+                    <input type="number" class="form-control" name="update_price"  value="{{$products->price}}">
                 </div>
                 <div class="col mb-3">
                     <label for="quantityinstock" class="form-label">Quantity Stock</label>
-                    <input type="text" class="form-control" name="update_quantityinstock"  value="{{$product->quantityinstock}}">
+                    <input type="text" class="form-control" name="update_quantityinstock"  value="{{$products->quantityinstock}}">
                 </div>
                 <div class="col mb-3 d-flex justify-content-center align-items-center">
                     <input type="hidden" name="id" value="{{$products->id}}">
